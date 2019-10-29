dd70607d70dd22179385da37d634d38421da3235
Matthew Hui <matt10145h@gmail.com>
2019-10-29 10:48:07 -0400

    finish exercise 1

--- src/change.js
+++ src/change.js
@@ -3,12 +3,23 @@ const readlineSync = require("readline-sync");
 const MIN = 0.01;
 const MAX = 0.99;
 
+let quarters = 0;
+let dimes = 0;
+let nickels = 0;
+let pennies = 0;
+
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
 if (Number.isNaN(amount)) {
-    // print your error message here
+  console.log("\nInvalid.")
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+    console.log("\nInvalid.")
 } else {
-    // write your code for making change here
+    convertPennies = amount * 100;
+    quarters = Math.floor(convertPennies/25);
+    dimes = Math.floor((convertPennies -= (quarters*25))/10);
+    nickels = Math.floor((convertPennies -= (dimes*10))/5);
+    pennies = Math.floor((convertPennies -= (nickels*5))/1);
+
+    console.log(`\n${quarters} quarters, ${dimes} dimes, ${nickels} nickels, ${pennies} pennies.`)
 }
